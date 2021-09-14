d08bc03d9a5fd0822a4b4ccdba835069a63469e7
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-09-14 18:17:33 -0500

    Seccion Confirmacion

--- Controllers/FormularioController.cs
+++ Controllers/FormularioController.cs
@@ -17,17 +17,24 @@ namespace Practica_2_lab.Controllers
             return View("Views/Formulario/Index.cshtml");
         }
         
+        [HttpPost]
         
         public IActionResult Guardar(string nombre , string apePat , string apeMat , string tipo , string telefono ,
         string distrito , string dire , string descripcion){
 
                Console.WriteLine(nombre);
 
-               return redirectToAction("Mostrar");
+               return RedirectToAction("Mostrar");
 
         }
 
-        
+        public IActionResult Mostrar() {
+             
+              return View("Views/Formulario/Confirmacion.cshtml");
+
+        }
+
+
 
     }
 }
