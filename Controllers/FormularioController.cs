d01dfa5c1e2bdd195a7fff147a256596420719f3
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-09-14 18:08:08 -0500

    Agregar Paginas y creacion de formulario

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica_2_lab.Models;
+
+namespace Practica_2_lab.Controllers
+{
+    public class FormularioController : Controller 
+    {
+
+        public IActionResult Index(){
+
+            return View("Views/Formulario/Index.cshtml");
+        }
+        
+        
+        public IActionResult Guardar(string nombre , string apePat , string apeMat , string tipo , string telefono ,
+        string distrito , string dire , string descripcion){
+
+               Console.WriteLine(nombre);
+
+               return redirectToAction("Mostrar");
+
+        }
+
+        
+
+    }
+}
