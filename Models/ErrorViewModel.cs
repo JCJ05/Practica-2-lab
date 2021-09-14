d7307d39f4faff9974faf5820064a2eac1b17834
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-09-14 17:09:57 -0500

    Creacion de Aplicacion MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Practica_2_lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
